f919d80974016c20636a313d1dd6e2b8e13454f2
basimkarhabi <basim.karhabi@gmail.com>
2023-02-26 10:31:22 +0100

    add reloaddocument

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -1,11 +1,12 @@
 
 import React from 'react'
-import {Routes,Route,Navigate}from 'react-router-dom'
+import {Routes,Route}from 'react-router-dom'
 import Home from "../pages/Home";
 import About from "../pages/About";
 import NotFound from "../pages/NotFound";
 import Contact from "../pages/Contact";
 import BookingForm from "../components/UI/BookingForm"
+
 
 const Routers = () => {
   return <Routes>
@@ -14,11 +15,11 @@ const Routers = () => {
           */}
       {/*<Route path="/home" element={<Home />} />*/}
       <Route path="/" element={<Home/>} reloadDocument/>
-      <Route path="/home" element={<Home />} />
-      <Route path="/about" element={<About />} />
+      <Route path="/home" element={<Home />} reloadDocument/>
+      <Route path="/about" element={<About />} reloadDocument/>
       <Route path="/contact" element={<Contact />} />
       <Route path="/BookingForm" element={<BookingForm />} />
-      <Route path="*" element={<div>Error 404 page noe found </div>} />  
+      <Route path="*" element={<NotFound />} />  
   </Routes>
   
 }
